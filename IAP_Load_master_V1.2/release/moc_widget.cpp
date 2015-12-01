5ca42dc19497bb37544f9d55b6c47c66c060be3c
mogu <503151695@qq.com>
2015-12-01 15:16:16 +0800

    IAP上位机路径变化 Signed-off-by: mogu <503151695@qq.com>

--- IAP_Load_master_V1.2/release/moc_widget.cpp
+++ IAP_Load_master_V1.2/release/moc_widget.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../IAP_Load_master/widget.h"
+#include "../../../IAP2/IAP_Load_master_V1.2/IAP_Load_master/widget.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
